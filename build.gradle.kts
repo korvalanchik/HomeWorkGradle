98927cec6815493efa5111c9d14870c5adebb98b
korvalanchik <korvalan@yahoo.com>
2023-11-12 17:48:21 +0200

    Gradle 1.0.0

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+// https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MyName"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
